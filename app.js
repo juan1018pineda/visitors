da0601489b4c67d823fa9ec404e38859bd513628
pineda.juan92 <pineda.juan92@gmail.com>
2022-01-17 20:46:55 -0500

    App that saves the info related to the users tha visit the page

--- app.js
+++ app.js
@@ -32,6 +32,4 @@ app.get("/", (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => console.log("Listening on port 3000"));
+app.listen(3000, () => console.log("Listening on port 3000"));
